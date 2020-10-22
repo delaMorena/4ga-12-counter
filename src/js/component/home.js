9e212945b10da2c2450f4a25574fd75c048fc448
delaMorena <martadmdo@gmail.com>
2020-10-22 13:04:39 +0000

    adding some more styles

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,7 +6,15 @@ import { Counter } from "./counter.js";
 //create your first component
 export function Home() {
 	return (
-		<div className="container-fluid">
+		<div className="jumbotron">
+			<h1 className="display-4">Hi Developer</h1>
+			<p className="lead">
+				Learning to code is hard...suuuuuper hard sometimes. You feel as
+				if you were loosing track of time and spending hours trying to
+				find out stuff. And you know what?? You actually are!!
+			</p>
+			<hr className="my-4" />
+
 			<div id="contador" className="row">
 				<Counter className="col-3" miliSec="1000" />
 				<Counter className="col-3" miliSec="10000" />
